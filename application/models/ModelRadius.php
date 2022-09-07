23440309af075a7a5a8acc163a809b7f0158ac01
riyansasaja <riyanboyuhu@gmail.com>
2022-09-07 09:21:50 +0800

    merubah penulisan model Radius

--- application/models/ModelRadius.php
+++ application/models/ModelRadius.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Modelradius extends CI_Model
+class ModelRadius extends CI_Model
 {
 
     public function getdatakelurahan($idkecamatan)
